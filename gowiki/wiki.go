910856941f096dc920bc7264353a660f857f141b
yuyahy <xvto.ya@gmail.com>
2024-08-20 12:43:09 +0000

    feat: The html/template packageまで実装

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,7 +1,11 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+
+	"html/template"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -27,9 +31,42 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	// templateのhtmlファイルから動的にhtmlを生成し、ResponseWriterへ書き込む
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	//fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "view", p)
+}
+
+// ページをロードし、HTML形式で表示する
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	// fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+	// 	"<form action=\"/save/%s\" method=\"POST\">"+
+	// 	"<textarea name=\"body\">%s</textarea><br>"+
+	// 	"<input type=\"submit\" value=\"Save\">"+
+	// 	"</form>",
+	// 	p.Title, p.Title, p.Body)
+	renderTemplate(w, "edit", p)
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	// p1.save()
+	// p2, _ := loadPage("TestPage")
+	// fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
