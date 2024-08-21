664ca499cb104426ab4ca1890f9a3810f18193fc
yuyahy <xvto.ya@gmail.com>
2024-08-21 12:55:47 +0000

    feat: Other tasks直前まで実装

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	// "fmt"
-
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
 type Page struct {
@@ -18,7 +18,8 @@ type Page struct {
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	// "0600"は作成するファイルのパーミッション
-	return os.WriteFile(filename, p.Body, 0600)
+	dir := "data/"
+	return os.WriteFile(dir+filename, p.Body, 0600)
 }
 
 // 与えられたタイトルのページをロードする関数
@@ -33,40 +34,84 @@ func loadPage(title string) (*Page, error) {
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	// templateのhtmlファイルから動的にhtmlを生成し、ResponseWriterへ書き込む
-	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)
+	// t, err := template.ParseFiles(tmpl + ".html")
+
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	// http.Error()は指定のHTTPレスポンスコードを返す(以下の例ではInternalServerError)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+	// err = t.Execute(w, p)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// }
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	//fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
 	renderTemplate(w, "view", p)
 }
 
 // ページをロードし、HTML形式で表示する
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	// fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-	// 	"<form action=\"/save/%s\" method=\"POST\">"+
-	// 	"<textarea name=\"body\">%s</textarea><br>"+
-	// 	"<input type=\"submit\" value=\"Save\">"+
-	// 	"</form>",
-	// 	p.Title, p.Title, p.Body)
 	renderTemplate(w, "edit", p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("invalid Page Title")
+	}
+	return m[2], nil // The title is the second subexpression
+}
+
+// 複数のテンプレートファイルを解析し、templatesへ格納する
+// なお、template.Must()を使用しているため、parseに失敗した時点でプログラムが終了する
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html"))
+
+// 正規表現をコンパイル
+// なお、MustCompileを使用しているため、コンパイルに失敗した時点でプログラムが終了する
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
 func main() {
-	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	// p1.save()
-	// p2, _ := loadPage("TestPage")
-	// fmt.Println(string(p2.Body))
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
